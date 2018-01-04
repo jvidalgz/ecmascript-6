ee694e36b727fb8d4dcce91c82501f2e4adbb2c2
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2018-01-04 10:07:39 -0400

    Corrige indentacion

--- es6-let.js
+++ es6-let.js
@@ -1,30 +1,29 @@
-
 /**
-* La sentencia let permite declarar una variable de alcance local
-*/
+ * La sentencia let permite declarar una variable de alcance local
+ */
 var fs = [];
 for (let i = 0; i < 10; i++) {
-  fs.push(function() {
-    console.log(i);
-  });
+    fs.push(function () {
+        console.log(i);
+    });
 }
 
-fs.forEach(function(f) {
-  f();
+fs.forEach(function (f) {
+    f();
 });
 
 
 /**
-* El mismo ejemplo, pero con sentencia var, permite que i sea modificada desde
-* dentro de la callback
-*/
+ * El mismo ejemplo, pero con sentencia var, permite que i sea modificada desde
+ * dentro de la callback
+ */
 var fs = [];
 for (var i = 0; i < 10; i++) {
-  fs.push(function() {
-    console.log(i);
-  });
+    fs.push(function () {
+        console.log(i);
+    });
 }
 
-fs.forEach(function(f){
-  f();
+fs.forEach(function (f) {
+    f();
 })
