0ea1de2c0a231e625f18c6ddcb120d06ab0684ab
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2018-01-04 09:47:50 -0400

    Ejemplo sentencia let

---
+++ es6-let.js
@@ -0,0 +1,30 @@
+
+/**
+* La sentencia let permite declarar una variable de alcance local
+*/
+var fs = [];
+for (let i = 0; i < 10; i++) {
+  fs.push(function() {
+    console.log(i);
+  });
+}
+
+fs.forEach(function(f) {
+  f();
+});
+
+
+/**
+* El mismo ejemplo, pero con sentencia var, permite que i sea modificada desde
+* dentro de la callback
+*/
+var fs = [];
+for (var i = 0; i < 10; i++) {
+  fs.push(function() {
+    console.log(i);
+  });
+}
+
+fs.forEach(function(f){
+  f();
+})
